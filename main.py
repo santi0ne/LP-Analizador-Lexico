b96351fd305648fda47f199aa52800db89612e6e
santi0ne <johsrodr@espol.edu.ec>
2023-11-05 23:18:02 -0500

    algoritmo de pruebas y mas tokens

--- main.py
+++ main.py
@@ -4,22 +4,61 @@
 
 import ply.lex as lex
 
+from pruebas import *
+
 # Palabras Reservadas
 reserved = {
-   'var':'VAR'
+   'var':'VAR',  # Johnny Rodriguez
+
+   'int':'INT',          # Johnny Rodriguez 
+   'float32':'FLOAT32',  # Johnny Rodriguez  
+   'float64':'FLOAT64',  # Johnny Rodriguez   
+   'string':'STRING',    # Johnny Rodriguez  
+   'bool':'BOOL',        # Johnny Rodriguez
+
+   'for':'FOR',          # Johnny Rodriguez
+   'if':'IF',            # Johnny Rodriguez
+   'else':'ELSE',        # Johnny Rodriguez
+   'func':'FUNC',        # Johnny Rodriguez
+   'return':'RETURN',    # Johnny Rodriguez
+   'switch':'SWITCH',    # Johnny Rodriguez
+   'case':'case'         # Johnny Rodriguez
 }
 
 # Secuencia de tokens
 tokens = (
-    'IDENTIFICADOR',
+    'IDENTIFICADOR',     # Johnny Rodriguez
+    'ENTERO',            # Johnny Rodriguez
+    'FLOTANTE',          # Johnny Rodriguez
+    'ASSIGN',            # Johnny Rodriguez
+    'LPAREN',            # Johnny Rodriguez
+    'RPAREN',            # Johnny Rodriguez
+    'LKEY',              # Johnny Rodriguez
+    'RKEY',              # Johnny Rodriguez
+    'COLLON'             # Johnny Rodriguez
 )+tuple(reserved.values())
 
 # Expresiones Regulares simples para símbolos
+t_ASSIGN = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LKEY = r'\{'
+t_RKEY = r'\}'
+t_COLLON = r':'
 
 # Expresiones Regulares para números y variables, incluye cast
-def t_IDENTIFICADOR(t):
+def t_IDENTIFICADOR(t):  # Johnny Rodriguez
     r'[a-z_]\w*'
     t.type = reserved.get(t.value,'IDENTIFICADOR')
+    return t
+
+def t_FLOTANTE(t):
+    r'-?\d*\.\d+'    
+    return t
+
+def t_ENTERO(t):
+    r'-?\d+'
+    t.value = int(t.value)    
     return t
 
 # Expresión regular para reconocer saltos de línea
@@ -39,7 +78,7 @@ def t_error(t):
 lexer = lex.lex()
 
 # Código para analizar
-code = "var 781 piloso"
+code = switch_declaration()
 
 # Enviando el código
 lexer.input(code)
---
+++ pruebas.py
@@ -0,0 +1,57 @@
+
+##### ALGORITMOS DE PRUEBA #####
+
+## Johnny Santiago Rodriguez Salinas
+
+def vars_declaration():
+    return '''
+            var cadena string      
+            var numero int
+            var numero1 float32
+            var numero2 float64
+            var bool1 bool
+            '''
+
+def for_body():
+    return '''
+           for i := 0; i < 5; i++ {
+           
+           }
+           '''
+
+def if_sin_else():
+    return '''
+           if (-8 == .9){
+        
+           }
+           '''
+
+def if_con_else():
+    return '''
+           if(-.7>.23){
+                return true
+           }else{
+                return false
+           }
+           '''
+
+def func_declaration():
+    return '''
+           func main(a int,b int){
+                return a + b 
+           }
+           '''
+
+def switch_declaration():
+    return '''
+           switch valor {
+                case 1:
+                    // codigo
+                case 2:
+                    // codigo
+                case 3:
+                    // codigo
+                default:
+                    // codigo
+            }
+           '''
