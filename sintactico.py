153f1f340b3e80e1ccf54208cca30218111b27d9
nicmacor <nicmacor@espol.edu.ec>
2023-11-13 00:25:22 -0500

    variable

--- sintactico.py
+++ sintactico.py
@@ -4,22 +4,36 @@ import ply.yacc as yacc
 from lexico import tokens
 
 def p_sentencia(p):
-    '''sentendia : impresion
-                | asignacion'''
+    '''sentencia : impresion
+                | asignacion
+                | variable
+                '''
+                
+def p_variable(p):
+    'variable : VAR IDENTIFICADOR ASSIGN valor'
+
 def p_asignacion(p):
-    "asignacion : IDENTIFICADOR ASSIGN valor"
+    "asignacion : IDENTIFICADOR ASSIGN valores"
 
 
 def p_impresion(p):
     "impresion : IMPRIMIR LPAREN valores RPAREN "
 
-def p_valor(p):
-    '''valor : ENTERO
-            | FLOTANTE'''
+def p_impresion_sin_valor(p):
+    'impresion : IMPRIMIR LPAREN RPAREN'
 
 def p_valores(p):
     '''valores : valor
-                | valor COMA valores '''
+                | valor COMA valores
+                '''
+def p_valor(p):
+    '''valor : ENTERO
+            | FLOTANTE
+            | STRING
+            | BOOL
+            | IDENTIFICADOR
+            '''
+    
 def p_error(p):
     print("Error de sintaxis")
 
@@ -28,7 +42,7 @@ parser = yacc.yacc()
 
 while True:
    try:
-       s = input('esp > ')
+       s = input('>>> ')
    except EOFError:
        break
    if not s: continue
